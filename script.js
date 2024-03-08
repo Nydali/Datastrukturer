569c2ba4be2943713c6061ab33bd9c08e362989c
Nydali <saints2812@gmail.com>
2024-03-08 12:21:45 +0100

    Datastructures

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript object containing different data types
+var myObject = {
+    string: "Hello, world!",
+    number: 42,
+    array: [1, 2, 3, 4, 5]
+};
+
+console.log(myObject);
+
+// Function that takes two numbers as parameters and performs a calculation
+function calculate(num1, num2) {
+    return num1 * num2; // Example calculation (multiplication)
+}
+
+console.log(calculate(5, 10));
+
+// Function that takes a string as a parameter and uses the "split" method
+function splitString(str) {
+    return str.split(' '); // Splitting the string by spaces
+}
+
+console.log(splitString("Hello world"));
